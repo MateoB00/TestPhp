e63eaea3ea1208528ec70ff49525a90fd0b27790
Matéo Bianco <95920842+MateoB91@users.noreply.github.com>
2022-02-18 16:07:34 +0100

    Add files via upload

--- controllers/modif-article-controller.php
+++ controllers/modif-article-controller.php
@@ -1,6 +1,14 @@
 <?php
 
+if (empty($_GET['id'])) error();
+
+require_once __DIR__ . '/../models/Article.php';
+$article = Article::retrieveByPK($_GET['id']);
+
+if (empty($article)) error();
+
 if (!empty($_POST)) {
+
     if (
         !empty($_POST['titre'])
         && !empty($_POST['contenu'])
@@ -10,10 +18,8 @@ if (!empty($_POST)) {
         && !empty($_POST['date_de_publication'])
         && validateDate($_POST['date_de_publication'])
         && substr($_POST['image'], -4) == '.jpg' || (substr($_POST['image'], -4) == '.png' || substr($_POST['image'], -5) == '.jpeg' || substr($_POST['image'], -4) == '.gif')
-    ) {
 
-        require_once __DIR__ . '/../models/Article.php';
-        $article = new Article;
+    ) {
         $article->titre = $_POST['titre'];
         $article->contenu = $_POST['contenu'];
         $article->image = $_POST['image'];
@@ -21,8 +27,9 @@ if (!empty($_POST)) {
         $article->date_de_publication = $_POST['date_de_publication'];
 
         $article->save();
-        redirection("liste-articles");
-    } else $error = "Vous avez mal rempli le formulaire";
+
+        redirection("home");
+    } else error();
 }
 
-require_once __DIR__ . '/../views/ajout-article.html.php';
+require_once __DIR__ . '/../views/modif-article.html.php';
