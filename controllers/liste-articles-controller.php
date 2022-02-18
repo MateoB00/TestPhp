ef0a28a488ba2c10c382744f736bcf1a3feadb56
Matéo Bianco <95920842+MateoB91@users.noreply.github.com>
2022-02-18 11:58:29 +0100

    Add files via upload

--- controllers/liste-articles-controller.php
+++ controllers/liste-articles-controller.php
@@ -2,4 +2,4 @@
 
 require_once __DIR__ . "/../models/Article.php";
 $articles = Article::all();
-require_once __DIR__ . "/../views/liste-articles.php";
+require_once __DIR__ . "/../views/liste-articles.html.php";
